54cd7943c86f2fa9d2f84eb22073cefefb8c10dc
Germán Rojas <garcejr@gmail.com>
2025-03-26 17:53:27 -0500

    feat: expand taskRoutes with additional endpoints for task retrieval and management

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,6 +4,17 @@ import * as taskController from '../controllers/taskController.js';
 const router = Router();
 
 router.get('/', taskController.getTasks);
-
+router.get("/:id", taskController.getTask);
+router.get("/status/:status", taskController.getTasksByStatus);
+router.get("/priority/:priority", taskController.getTasksByPriority);
+router.get("/completed/:completed", taskController.getTasksByCompletion);
+router.get("/date", taskController.getTasksByDate);
+router.get("/date/:dueDate", taskController.getTasksByDate);
+router.get("/date/:createdAt", taskController.getTasksByCreationDate);
+router.post("/date", taskController.getTasksInDateRange);
+router.post("/", taskController.createTask);
+router.post("/:id/markComplete", taskController.markTaskAsCompleted);
+router.put("/:id", taskController.updateTask);
+router.delete("/:id", taskController.deleteTask);
 
 export default router;
