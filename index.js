c70be0ab672cf54edc0524e0483807efce03c14a
Germán Rojas <garcejr@gmail.com>
2025-03-26 11:24:34 -0500

    feat: integrate taskRoutes for task-related API handling

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import connectDB from './config/db.js'
 import dotenv from 'dotenv'
+import taskRoutes from './routes/taskRoutes.js'
+
 dotenv.config()
 
 connectDB()
@@ -11,6 +13,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/tasks', taskRoutes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
